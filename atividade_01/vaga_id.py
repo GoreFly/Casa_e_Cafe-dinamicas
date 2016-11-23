87612a967dcee2cdea171ceeb47091e1df429378
Pedro Garcia <garcia.ph@gmail.com>
2016-11-23 00:33:43 -0200

    Código Final Funcional

--- atividade_01/vaga_id.py
+++ atividade_01/vaga_id.py
@@ -2,13 +2,19 @@
 Nov 11 2016 11:15PM
 
 @author: Pedro Garcia
+@args: ID do usuário consultado
+@return: número de vagas para o usuário com o ID informado
 """
 
 
 import json
 import requests
+import sys
 
-
-url = "http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com"
+# monta a url correta com o id de usuário fornecido para fazer a consulta
+url = "http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/" + sys.argv[1] + "/opportunities"
 response = requests.get(url)
-print(response.json())
+data = response.json()
+
+# imprime no console o número de entradas para o usuário, sendo cada entrada uma vaga
+print (len(data))
