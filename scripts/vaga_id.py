eed673fc70d392e7e2992d9fa2e73755a907a1f4
Pedro Garcia <garcia.ph@gmail.com>
2016-11-25 11:48:02 -0200

    Tratamento da Falta de ID
    Tratamento da exceção causada quando o ID do usuário não é fornecido por
    parâmetro

--- scripts/vaga_id.py
+++ scripts/vaga_id.py
@@ -11,10 +11,15 @@ import json
 import requests
 import sys
 
-# monta a url correta com o id de usuário fornecido para fazer a consulta
-url = "http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/" + sys.argv[1] + "/opportunities"
-response = requests.get(url)
-data = response.json()
+try:
+    # monta a url correta com o id de usuário fornecido para fazer a consulta
+    url = "http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/" + sys.argv[1] + "/opportunities"
+    response = requests.get(url)
+    data = response.json()
+    # imprime no console o número de entradas para o usuário, sendo cada entrada uma vaga
+    print (len(data))
 
-# imprime no console o número de entradas para o usuário, sendo cada entrada uma vaga
-print (len(data))
+except IndexError:
+    # verifica se o id do usuário foi fornecido
+    print("ERRO: ID do usuario nao fornecido. Por favor, forneça o ID do usuario como parametro.")
+    print("\tpython vaga_id.py [ID]")
